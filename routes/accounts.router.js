6ee26eeb7888d44f672770e5ca412e2e2c62e08d
Michael Johnson <michael@icefoganalytics.com>
2022-06-16 14:07:06 -0700

    Update accounts.router.js

--- routes/accounts.router.js
+++ routes/accounts.router.js
@@ -4,7 +4,7 @@ const knex = require('../data')
 const accountsRouter = express.Router()
 module.exports = accountsRouter
 
-accountsRouter.post("/departments", async (req, res) => {
+accountsRouter.get("/departments", async (req, res) => {
   let results = await knex("EDW-Finance-Stage.dbo.GLAcctTree")
     .select(["account as dept", "descr"])
     .where({ org: "YUKON", level: 1 })
